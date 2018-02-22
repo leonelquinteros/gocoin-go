33dc458c287c7d6accdcc25c05aa324dbfa1e7d7
Leonel Quinteros <leonel.quinteros@gmail.com>
2018-02-22 15:16:16 -0300

    Add InvoiceWebhook type to handle invoice webhook message payloads

--- webhooks.go
+++ webhooks.go
@@ -6,3 +6,10 @@ type Webhook struct {
 	Event   string      `json:"event"`
 	Payload interface{} `json:"payload"`
 }
+
+// InvoiceWebhook is the type that contain the expected invoice webhook payload
+type InvoiceWebhook struct {
+	ID      string  `json:"id"`
+	Event   string  `json:"event"`
+	Payload Invoice `json:"payload"`
+}
